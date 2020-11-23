d7d2b043693b770d1be5ac26a8a01e78539cf139
dasouche <dasouche@wangchunyan0197.lan>
2020-11-23 23:20:22 +0800

    update website

--- examples/website/src/utils/tpl.js
+++ examples/website/src/utils/tpl.js
@@ -11,7 +11,7 @@ export default (code, { dependences = [] } = {}) =>  `
     <script src="/observer/dist/main.js"></script>`;
         } else {
             return `<script src="https://unpkg.com/@vanilla-jsx/jsx-runtime@1.2.3/dist/main.js"></script>
-    <script src="https://unpkg.com/@vanilla-jsx/observer@1.3.1/dist/main.js"></script>`;
+    <script src="https://unpkg.com/@vanilla-jsx/observer@1.3.2/dist/main.js"></script>`;
         }
     })()}
     ${dependences.map(ele => `<script src="${ele}"></script>`)}
