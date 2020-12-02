62471d9a493d9f66de9a28615f432e0ef4fea306
boltdoggy <github@robolt.uu.me>
2020-12-03 01:00:05 +0800

    ppt

---
+++ examples/ppt/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    devServer: {
+        publicPath: '/dist'
+    }
+}
